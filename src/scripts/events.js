592762d6e7684af2da31d23ab959c689eee95587
Daniil <daniil210910ee@gmail.com>
2019-11-20 20:27:16 +0200

    Events

---
+++ src/scripts/events.js
@@ -0,0 +1,34 @@
+function inputInfo() {
+    console.log("text");
+}
+
+function buttonClick() {
+    alert("You clicked me");
+}
+
+function overHeader() {
+    alert("This is header");
+}
+
+function outHeader() {
+    alert("You left header");
+}
+
+function pushKey() {
+    alert("You push key");
+}
+
+function pageLoaded() {
+    alert("Page is loaded");
+}
+
+
+
+
+
+window.inputInfo = inputInfo;
+window.buttonClick = buttonClick;
+window.overHeader = overHeader;
+window.outHeader = outHeader;
+window.pushKey = pushKey;
+window.pageLoaded = pageLoaded;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,19 @@
 import '../styles/index.scss';
+import './events.js';
 
-const user = {
-    name: "Daniil",
-    lastName: "Kravchenko",
-    age: 22,
-    dob: "28.05.1997",
-    height: 180,
-    hasPet: true,
-    favoriteFood: ["pizza", "burgers", "steak"],
-    userInformation: function () {
-        console.log("Name:" + user.name + " " + "LastName:" + user.lastName + " " + "Age:" + user.age);
-    }
-};
+// const user = {
+//     name: "Daniil",
+//     lastName: "Kravchenko",
+//     age: 22,
+//     dob: "28.05.1997",
+//     height: 180,
+//     hasPet: true,
+//     favoriteFood: ["pizza", "burgers", "steak"],
+//     userInformation: function () {
+//         console.log("Name:" + user.name + " " + "LastName:" + user.lastName + " " + "Age:" + user.age);
+//     }
+// };
 
-console.log(user);
-user.userInformation();
+// console.log(user);
+// user.userInformation();
 
